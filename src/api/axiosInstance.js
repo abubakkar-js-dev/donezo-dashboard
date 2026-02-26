ec25941b3e97673a20e2f250f40f4c59c093925f
abubakkar-js-dev <mdabubakkars182@gmail.com>
2026-02-26 19:30:09 +0600

    add: axios setup done

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+// Attach Token
+
+api.interceptors.request.use((config)=>{
+    const token = localStorage.getItem('token');
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`;
+    };
+    return config;
+},(err)=>{
+    return Promise.reject(err);
+})
+
+
+// Auto-logout on 401 and 403
+
+api.interceptors.response.use(
+    (res)=> res
+,(err)=>{
+    const status = err.response.status;
+    // console.log(status)
+    if(status === 401 || status === 403){
+        // log out user
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+    return Promise.reject(err);
+})
+
+export default api;
